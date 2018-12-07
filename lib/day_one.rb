0ac497cfaf5785b84b71e4299998d0f133676a8b
Colin Kennedy <colin.kennedy@intercom.io>
2018-12-07 14:56:08 +0000

    Day One solutions

--- lib/day_one.rb
+++ lib/day_one.rb
@@ -1,3 +1,36 @@
 class DayOne
 
+  def self.find_frequency(frequencies:)
+    frequency = 0
+    frequencies.each do |change|
+      frequency += change
+    end
+    frequency
+  end
+
+  def self.find_repeating_frequency(frequencies:, frequency: 0, occurences: { 0 => 1})
+    frequencies.each do |change|
+      frequency += change
+      return frequency if occurences[frequency] != nil
+      occurences[frequency] = 1
+    end
+
+    find_repeating_frequency(frequencies: frequencies, frequency: frequency, occurences: occurences)
+  end
 end
+
+input = []
+
+File.open("input/day_one", "r") do |f|
+  f.each_line do |line|
+    input.push(line.to_i)
+  end
+end
+
+frequency = DayOne.find_frequency(frequencies: input)
+
+puts "Frequency: #{frequency}"
+
+repeating_frequency = DayOne.find_repeating_frequency(frequencies: input)
+
+puts "Repeating frequency #{repeating_frequency}"
--- spec/day_one_spec.rb
+++ spec/day_one_spec.rb
@@ -1,5 +1,55 @@
 require "day_one"
 
 describe DayOne do
+  describe "#find_frequency" do
+    context "with an input of 1 and 1" do
+      it "returns 2" do
+        expect(DayOne.find_frequency(frequencies: [1, 1])).to eq(2)
+      end
+    end
 
+    context "with an input of 1 and -1" do
+      it "returns 0" do
+        expect(DayOne.find_frequency(frequencies: [1, -1])).to eq(0)
+      end
+    end
+
+    context "with an input of 1 and -2 and 10" do
+      it "returns 9" do
+        expect(DayOne.find_frequency(frequencies: [1, -2, 10])).to eq(9)
+      end
+    end
+  end
+
+  describe "#find_repeating_frequency" do
+    context "with an input of 1 and -1" do
+      it "returns 0" do
+        expect(DayOne.find_repeating_frequency(frequencies: [1, -1])).to eq(0)
+      end
+    end
+
+    context "with an input of 1 and 1 and 2 and -2 and 8" do
+      it "returns 2" do
+        expect(DayOne.find_repeating_frequency(frequencies: [1, 1, 2, -2, 8])).to eq(2)
+      end
+    end
+
+    context "with an input of +3, +3, +4, -2, -4" do
+      it "returns 10" do
+        expect(DayOne.find_repeating_frequency(frequencies: [+3, +3, +4, -2, -4])).to eq(10)
+      end
+    end
+
+    context "with an input of -6, +3, +8, +5, -6" do
+      it "returns 10" do
+        expect(DayOne.find_repeating_frequency(frequencies: [-6, +3, +8, +5, -6])).to eq(5)
+      end
+    end
+
+    context "with an input of +7, +7, -2, -7, -4" do
+      it "returns 10" do
+        expect(DayOne.find_repeating_frequency(frequencies: [+7, +7, -2, -7, -4])).to eq(14)
+      end
+    end
+  end
 end
