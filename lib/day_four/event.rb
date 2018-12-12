7be92c2ef3a1b8220968018b02adf834e39048f9
Colin Kennedy <colin.kennedy@intercom.io>
2018-12-12 17:18:33 +0000

    Add event parsing class

--- lib/day_four/event.rb
+++ lib/day_four/event.rb
@@ -1,5 +1,18 @@
-module DayFour
-  class Event
+require 'time'
 
+module DayFour
+
+  TIME_AND_MESSAGE_REGEX = /(\[[0-9-:\s]+\])\s(.*)/
+  GUARD_ID_REGEX = /#([0-9]+)/
+
+  class Event
+    attr_reader :time, :message, :guard
+
+    def initialize(data)
+      time_and_message = data.match(TIME_AND_MESSAGE_REGEX)
+      @time = Time.parse(time_and_message[1])
+      @message = time_and_message[2]
+      @guard = data.match(GUARD_ID_REGEX)[1].to_i if data.match?(GUARD_ID_REGEX)
+    end
   end
 end
--- spec/day_four/event_spec.rb
+++ spec/day_four/event_spec.rb
@@ -1,13 +1,47 @@
 require "day_four/event"
+require "time"
 
 describe ::DayFour::Event do
-  context "with an input like [1518-09-16 23:57] Guard #1889 begins shift" do
-    let(:raw_event) { "[1518-09-16 23:57] Guard #1889 begins shift" }
-    let(:event) { ::DayFour::Event.new(raw_event) }
+  let(:raw_event) { "#{time} #{message}" }
+  let(:event) { ::DayFour::Event.new(raw_event) }
 
+  shared_examples "creates an event with message" do
     it "instantiates a class with the correct timestamp" do
-      expect(event).to be_instance_of(::DayThree::Event)
-      expect(event.time).to eq(1518-09-16 23:57)
+      expect(event).to be_instance_of(::DayFour::Event)
+      expect(event.time).to eq(Time.parse(time))
     end
+
+    it "instantiates a class with the correct message" do
+      expect(event.message).to eq(message)
+    end
+  end
+
+  context "with an input like [1518-09-16 23:57] Guard #1889 begins shift" do
+    let(:time) { "[1518-09-16 23:57]" }
+    let(:message) { "Guard #1889 begins shift" }
+
+    include_examples "creates an event with message"
+
+    it "has the guard id set" do
+      expect(event.guard).to eq(1889)
+    end
+  end
+
+  context "with an input like [1518-05-27 00:47] wakes up" do
+    let(:time) { "[1518-05-27 00:47]" }
+    let(:message) { "wakes up" }
+
+    include_examples "creates an event with message"
+
+    it "has no guard id set" do
+      expect(event.guard).to be_nil
+    end
+  end
+
+  context "with an input like [1518-09-26 00:48] falls asleep" do
+    let(:time) { "[1518-09-26 00:48]" }
+    let(:message) { "falls asleep" }
+
+    include_examples "creates an event with message"
   end
 end
