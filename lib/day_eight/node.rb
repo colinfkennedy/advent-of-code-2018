04e83689d63a13a6952d681e5115ba8a151da873
Colin Kennedy <colin.kennedy@intercom.io>
2018-12-21 12:14:38 +0000

    Day Eight, nodes working

--- lib/day_eight/node.rb
+++ lib/day_eight/node.rb
@@ -2,9 +2,19 @@ module DayEight
   class Node
     attr_reader :children, :metadata
 
-    def initialize(raw_input)
-      @children = []
-      @metadata = []
+    def initialize(children:, metadata:)
+      @children = children
+      @metadata = metadata
+    end
+
+    def self.process_raw_input(raw_input)
+      number_of_children = raw_input.shift
+      number_of_metadata_entries = raw_input.shift
+      children = []
+      (0...number_of_children).each do |i|
+        children.push(process_raw_input(raw_input))
+      end
+      Node.new(children: children, metadata: raw_input.shift(number_of_metadata_entries))
     end
   end
 end
--- spec/day_eight/node_spec.rb
+++ spec/day_eight/node_spec.rb
@@ -2,7 +2,7 @@ require "day_eight/node"
 
 describe ::DayEight::Node do
   let(:raw_input) { [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2] }
-  let(:node) { described_class.new(raw_input) }
+  let(:node) { described_class.process_raw_input(raw_input) }
 
   it "has 2 children and 3 metadata entries" do
     expect(node.children.size).to eq(2)
